afc1d8dfcbeb72d22054710ebc59ea98fe901718
xss <307894665@qq.com>
2022-03-05 12:42:50 +0800

    aaa

--- src/main/java/gz/sw/SwApplication.java
+++ src/main/java/gz/sw/SwApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SwApplication {
+public class SwApplication { 
 
     public static void main(String[] args) {
         SpringApplication.run(SwApplication.class, args);
